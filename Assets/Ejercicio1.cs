9d52846bf3b11a004f43187f1f8249f5c1ca2c96
BrandonStock1 <46919113@est.ort.edu.ar>
2022-04-25 12:08:31 -0300

    codigo ex 1 y 2

---
+++ Assets/Ejercicio1.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Ejercicio1 : MonoBehaviour
+{
+
+    public int edad;
+    public Text texto;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (edad >= 18)
+        {
+            Debug.Log("Es mayor de edad");
+            texto.text = "Es mayor de edad";
+        }
+        else
+        {
+            Debug.Log("No es mayor de edad");
+            texto.text = "No es mayor de edad";
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
+
---
+++ Assets/Ejercicio2.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Ejercicio2 : MonoBehaviour
+{
+
+    string contraseñacor;
+    string contraseña;
+    public Text texto;
+    public Text ingreso;
+    public GameObject mensaje;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        contraseñacor = "12345";
+        mensaje.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void validarContrasenia()
+    {
+        contraseña = ingreso.text;
+        if (contraseña == contraseñacor)
+        {
+            mensaje.SetActive(true);
+            texto.text = "Bienvenido";
+            Debug.Log("Bienvenido");
+        }
+        else
+        {
+            mensaje.SetActive(true);
+            texto.text = "Contraseña incorrecta";
+            Debug.Log("Contraseña incorrecta");
+        }
+
+
+    }
+}
